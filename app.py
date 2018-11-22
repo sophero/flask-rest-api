089d92d8fa8ea960858cfa163dae7377788444dd
Sophia Di <backwardsxmarathon@gmail.com>
2018-11-21 22:25:57 -0500

    Add GET and POST routes

--- app.py
+++ app.py
@@ -13,33 +13,62 @@ stores = [
   }
 ]
 
+# 									 #
+#   Store Endpoints	 #
+# 									 #
+
 # POST /store {name:}
 @app.route('/store', methods=['POST'])
-def create_store():
-  request_data = request.get_json() # I don't like how request is just mysteriously there without you declaring it like you would in Node. Nice to have learned these principles in JavaScript.
+def create_store(name):
+  request_data = request.get_json()
   new_store = {
     'name': request_data['name'],
     'items': []
   }
   stores.append(new_store)
   return jsonify(new_store) # Need to use jsonify! otherwise you're returning a dictionary and you need to return a string. json is a string... wow I totally forgot. JSONView lyf
-  
-
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
-  pass
+  # iterate over stores
+  for store in stores:
+  # if the store name matches, return it
+    if store.name == name
+      return store
+  # if none match, return an error message
+  return jsonify('message': 'No stores found with name {}'.format(name))
 
-@app.route('/store')
+# GET /stores
+@app.route('/stores')
 def get_stores():
   return jsonify({'stores': stores})
 
+# 									 #
+#   Item Endpoints	 #
+# 									 #
+
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store():
-  pass
+def create_item_in_store(name):
+  req_data = request.get_json()
+  for store in stores():
+    if store['name'] == name:
+      new_item = {
+        'name': req_data['name'],
+        'price': req_data['price']
+      }
+      store.items.append(new_item)
+      return jsonify(new_item) # new_item is an object, woohoo!
+	return jsonify({ 'message': 'No store found with name {}'.format(name) })
 
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+  for store in stores():
+    if store['name'] == name:
+      return jsonify({ 'items': store['items'] })
+  return jsonify({ 'message': 'No store found with name {}'.format(name) })
 
 
 app.run(port=2401)
